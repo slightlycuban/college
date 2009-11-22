14297d7cecb6ad39e925ab09bbb096d903690a31
Michael Tracy <mikeatracy@gmail.com>
2009-11-21 19:57:14 -0500

    Tried to implement mergesort--its not working yet.

--- src/sorts/Sort.java
+++ src/sorts/Sort.java
@@ -11,7 +11,8 @@ package sorts;
  */
 public final class Sort {
 
-    public static void insertion (int[] array) {
+    public static int[] insertion (int[] toBeSorted) {
+        int[] array = toBeSorted;
         for (int place = 1; place < array.length; place ++) {
             int temp = array[place];
             int shift = place;
@@ -20,44 +21,47 @@ public final class Sort {
             }
             array[shift] = temp;
         }
+        return array;
     }
 
-    private static void merge (int[] array, int[] temp, int start, int end) {
-        int length = end - start;
-        int middle = start + length;
-        if (length <= 1) return;
-        else {
-            int p1 = start;
-            int p2 = middle;
-            int next = start;
-            while (p1 != middle || p2 != end) {
-                if (array[p1] < array[p2]) {
-                    temp[next] = array[p1];
-                    p1++;
-                } else if(array[p2] < array[p1]) {
-                    temp[next] = array[p2];
-                    p2++;
-                }
-                else {
-                    temp[next] = array[p1];
-                    next++;
-                    temp[next] = array[p2];
-                    p1++;
-                    p2++;
-                }
-                next++;
+    private static void merge (int[] array, int[] temp, int start, int middle, int end) {
+        System.out.println("Status of temp:");
+        for (int i : temp) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        //int length = (start + end) / 2;
+        int leftPos = start;
+        int rightPos = middle + 1;
+        int next = start;
+        while (leftPos <= middle && rightPos <= end) {
+            if (array[leftPos] <= array[rightPos]) {
+                temp[next++] = array[leftPos++];
+            } else {
+                temp[next++] = array[rightPos++];
             }
+        }
+        while (rightPos <= end) {
+            temp[next++] = array[rightPos++];
+        }
+        while (leftPos <= middle) {
+            temp[next++] = array[leftPos++];
         }
     }
 
     private static void mergesort (int[] array, int[] temp, int start, int end) {
-
+        if (start < end) {
+            int middle = (end + start)/ 2;
+            mergesort(array, temp, start, middle);
+            mergesort(array, temp, middle + 1, end);
+            Sort.merge(array, temp, start, middle, end);
+        }
     }
 
-    public static void mergesort (int[] array) {
+    public static int[] mergesort (int[] array) {
         int[] temp = new int[array.length];
         Sort.mergesort(array, temp, 0, array.length - 1);
-        array = temp;
+        return temp;
     }
 
     
