018dc07602b9845f327a7ab1a4001608a225f077
Michael Tracy <mikeatracy@gmail.com>
2011-04-10 12:35:43 -0400

    Change control loop in server.c to switch from if-else
    	Update .gitignore

--- server.c
+++ server.c
@@ -89,6 +89,9 @@ int main(int argc, char *argv[]) {
 	while (bytes_sent < len)
 		bytes_sent += send(client, welcome, len, 0);
 
+	/*
+	 * Main control loop
+	 */
 	//char command[5];
 	char * command;
 	while ((strcmp(command, "EXIT") != 0) && (strcmp(command,"exit") != 0 )) {
@@ -96,116 +99,151 @@ int main(int argc, char *argv[]) {
 		short nmsg_len, hmsg_len;
 		command = recv_mesg(client);
 		// printf("Command is to %s.\n", command);
-		if ( (command[0] == 'E') || (command[0] == 'e')) {
-			char qmesg[] = "Server disconnecting client";
-			short hmesg_len = strlen(qmesg);
-			short nmesg_len = htons(hmesg_len);
-			bytes_sent = 0;
-			while (bytes_sent < sizeof(short))
-				send(client, &nmesg_len, sizeof(short), 0);
-			bytes_sent = 0;
-			while (bytes_sent < hmesg_len)
-				send(client, qmesg, hmesg_len, 0);
-			free(command);
-			break;
-		}
-		else if ( (command[0] == 'L') || (command[0] == 'l')) {
+		switch (command[0]) {
+			/*
+			 * EXIT
+			 * Disconnect client
+			 */
+			case 'E': case 'e':
+				{
+					char qmesg[] = "Server disconnecting client";
+					short hmesg_len = strlen(qmesg);
+					short nmesg_len = htons(hmesg_len);
+					bytes_sent = 0;
+					while (bytes_sent < sizeof(short))
+						send(client, &nmesg_len, sizeof(short), 0);
+					bytes_sent = 0;
+					while (bytes_sent < hmesg_len)
+						send(client, qmesg, hmesg_len, 0);
+					free(command);
+				}
 
-			printf("Server received LIST command.\n");
-			char * path = recv_mesg(client);
-			printf("Path: %s\n", path);
-			char * result = list(path, 4096);
-			puts(result);
-			send_mesg(client, result, strlen(result));
-			free(path);
-			free(result);
+				break;
 
-		}
-		else if ( (command[0] == 'R') || (command[0] == 'r')) {
+			/*
+			 * LS, or LIST
+			 * List all files in DIR
+			 */
+			case 'L':
+			case 'l':
+				{
+					printf("Server received LIST command.\n");
+					char * path = recv_mesg(client);
+					printf("Path: %s\n", path);
+					char * result = list(path, 4096);
+					puts(result);
+					send_mesg(client, result, strlen(result));
+					free(path);
+					free(result);
+				}
 
+				break;
+				
+			/*
+			 * RETR or Retrieve
+			 * get a file from the server
+			 */
+			case 'R':
+			case 'r':
+				{
+					printf("Server got RETR command\n");
+					char * f_name = recv_mesg(client);
 
-			printf("Server got RETR command\n");
-			char * f_name = recv_mesg(client);
+					int in;
+					if ((in = open(f_name, O_RDONLY)) < 0) {
+						perror("server cannot open file");
+						send_mesg(client, "-1", 2);
+						continue;
+					}
+					send_mesg(client, "0", 1);
+					printf("Server opened file.\n");
+					char * status= recv_mesg(client);
+					if (status[0] != '0') {
+						perror("client couldn't do that thing with the file");
+						continue;
+					}
+					printf("Client opened file.\n");
+					struct stat fileinfo;
+					fstat(in, &fileinfo);
+					int f_size = fileinfo.st_size;
+					char * size_buff = malloc(sizeof(char) * 10);
+					sprintf(size_buff, "%d", f_size);
+					send_mesg(client, size_buff, 10);
+					char * buff = malloc(sizeof(char) * 512);
+					int bytes;
+					while ((bytes = read(in, buff, 512)) > 0) {
+						printf("Trying to send %d bytes\n", bytes);
+						send(client, buff, bytes, 0);
+						memset(buff, '\0', 512);
+					}
+					free(size_buff);
+					free(buff);
+				}
 
-			int in;
-			if ((in = open(f_name, O_RDONLY)) < 0) {
-				perror("server cannot open file");
-				send_mesg(client, "-1", 2);
-				continue;
-			}
-			send_mesg(client, "0", 1);
-			printf("Server opened file.\n");
-			char * status= recv_mesg(client);
-			if (status[0] != '0') {
-				perror("client couldn't do that thing with the file");
-				continue;
-			}
-			printf("Client opened file.\n");
-			struct stat fileinfo;
-			fstat(in, &fileinfo);
-			int f_size = fileinfo.st_size;
-			char * size_buff = malloc(sizeof(char) * 10);
-			sprintf(size_buff, "%d", f_size);
-			send_mesg(client, size_buff, 10);
-			char * buff = malloc(sizeof(char) * 512);
-			int bytes;
-			while ((bytes = read(in, buff, 512)) > 0) {
-				printf("Trying to send %d bytes\n", bytes);
-				send(client, buff, bytes, 0);
-				memset(buff, '\0', 512);
-			}
-			free(size_buff);
-			free(buff);
+				break;
 
-		}
-		else if ( (command[0] == 'S') || (command[0] == 's')) {
+			/*
+			 * ST or STOR
+			 * Store or put a file on the server in DIR
+			 */
+			case 'S':
+			case 's':
+				{
+					printf("Server got STOR command\n");
+					char * f_name = recv_mesg(client);
+					printf("Opening %s\n", f_name);
+					int out;
+					if ((out = open(f_name, O_WRONLY | O_CREAT | O_EXCL, 0644)) < 0) {
+						perror("server cannot open file");
+						send_mesg(client, "-1", 2);
+						continue;
+					}
+					send_mesg(client, "0", 1);
+					char * size_buff = recv_mesg(client);
+					int f_size = atoi(size_buff);
+					free(size_buff);
+					char *buff = malloc(sizeof(char) * 512);
+					int bytes_recv = 0;
+					while (bytes_recv < f_size) {
+						memset(buff, '\0', 512);
+						bytes_recv += recv(client, buff, 512, 0);
+						printf("got %d bytes\n", bytes_recv);
+						write(out, buff, strlen(buff));
+					}
+					printf("Done recieving file\n");
 
-			printf("Server got STOR command\n");
-			char * f_name = recv_mesg(client);
-			printf("Opening %s\n", f_name);
-			int out;
-			if ((out = open(f_name, O_WRONLY | O_CREAT | O_EXCL, 0644)) < 0) {
-				perror("server cannot open file");
-				send_mesg(client, "-1", 2);
-				continue;
-			}
-			send_mesg(client, "0", 1);
-			char * size_buff = recv_mesg(client);
-			int f_size = atoi(size_buff);
-			free(size_buff);
-			char *buff = malloc(sizeof(char) * 512);
-			int bytes_recv = 0;
-			while (bytes_recv < f_size) {
-				memset(buff, '\0', 512);
-				bytes_recv += recv(client, buff, 512, 0);
-				printf("got %d bytes\n", bytes_recv);
-				write(out, buff, strlen(buff));
-			}
-			printf("Done recieving file\n");
+					free(buff);
+					close(out);
+				}
 
-			free(buff);
-			close(out);
+				break;
 
-		}
-		else if ( (command[0] == 'C') || (command[0] == 'c')) {
+			/*
+			 * CD or CHDIR
+			 * Switch to new director DIR
+			 */
+			case 'C':
+			case 'c':
+				{
+					printf("Server received CD command.\n");
+					char * path = recv_mesg(client);
+					printf("Path: %s\n", path);
+					if (chdir(path) != 0) {
+						char *error = "Path not changed.\n";
+						perror(error);
+						send_mesg(client, error, strlen(error));
+					} else {
+						char *success ="Path changed.\n";
+						send_mesg(client, success, strlen(success));
+					}
+					free(path);
+				}
 
-			printf("Server received CD command.\n");
-			char * path = recv_mesg(client);
-			printf("Path: %s\n", path);
-			if (chdir(path) != 0) {
-				char *error = "Path not changed.\n";
-				perror(error);
-				send_mesg(client, error, strlen(error));
-			} else {
-				char *success ="Path changed.\n";
-				send_mesg(client, success, strlen(success));
-			}
-			free(path);
+				break;
 
-		}
-		else {
-			printf("command not supported\n");
-			
+			default:
+				printf("command not supported\n");
+				break;
 		}
 		
 		free(command);
